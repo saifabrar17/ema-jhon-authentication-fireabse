8b61414af76ecb681356357d2095fb14bcd1d6ea
Saif Abrar <62766337+saifabrar17@users.noreply.github.com>
2022-04-27 21:50:36 +0600

    commiting changes

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -7,8 +7,30 @@ import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [products, setProducts] = useProducts();
+    
     const [cart, setCart] = useState([]);
+    const [pageCount, setPageCount] = useState(0);
+    const [page, setPage] = useState(0); 
+    const [size, setSize] = useState(10); 
+    const [products, setProducts] = useState([]);
+
+    useEffect( () =>{
+        fetch(`http://localhost:5000/product?page=${page}&size${size}`)
+        .then(res => res.json())
+        .then(data => setProducts(data));
+    }, []);
+
+
+    useEffect(()=>{
+        fetch('http://localhost:5000/productCount')
+        .then(res => res.json())
+        .then(data => {
+            const count = data.count;
+            const pages = Math.ceil(count/10);
+            setPageCount(pages);  
+        })
+    },[])
+
 
     useEffect( () =>{
         const storedCart = getStoredCart();
@@ -52,6 +74,21 @@ const Shop = () => {
                         handleAddToCart={handleAddToCart}
                         ></Product>)
                 }
+                <div className='pagination'>
+                    {
+                        [...Array(pageCount).keys()]
+                        .map(number => <button
+                            className={page === number ? 'selected' : ''}
+                            onClick={()=> setPage(number)}
+                        >{number+1}</button>)
+                    }
+                    <select onChange={e => setSize(e.target.value)}>
+                        <option value="5">5</option>
+                        <option value="10" selected>10</option>
+                        <option value="15">15</option>
+                        <option value="20">20</option>
+                    </select>
+                </div>
             </div>
             <div className="cart-container">
                 <Cart cart={cart}>
